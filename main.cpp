07e71465a27a2f7cf6058a11ce7d573fef3b04a3
MahdiGhorbani8 <92153225+MahdiGhorbani8@users.noreply.github.com>
2021-10-08 14:05:26 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
-
+#include <conio.h>
+#include <math.h>
 using namespace std;
-int main()
+main()
 {
+    float weight,height,bmi;
+    cout << "Enter your weight(Kg) and height(meter): "<<endl;
+    cin>>weight>>height;
+    bmi=weight/(height*height);
+    cout<<"BMI: "<<bmi<<endl;
+    if(bmi<18)
+            cout<<"Thin";
+    else if(bmi>=18 && bmi<25)
+            cout<<"Normal";
+    else if(bmi>=25 && bmi<30)
+            cout<<"Fat";
+    else
+            cout<<"Out of range";
 
- int a,b,c;
- cin>>a>>b>>c;
- if(a+b>c && b+c>a && a+c>b)
-  cout<<"YES";
- else
-  cout<<"NO";
-
+    getch();
 }
+
+
